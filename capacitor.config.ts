5d36d2ee23e2e7e9e7010aa5aaadebbac8beb27e
Webphotographydesign <80162274+Webphotographydesign@users.noreply.github.com>
2025-03-27 20:50:07 +0100

    change BundelID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'de.tenne.tenneapp',
-  appName: 'tenne-app',
+  appName: 'TENNE',
   webDir: 'www'
 };
 
